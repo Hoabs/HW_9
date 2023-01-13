33e915525bd2497f1b5166ed5d42502005b8d3fd
Arsen <hoabs@mail.ru>
2023-01-13 17:38:36 +0300

    Done

---
+++ HW_9.1/Program.cs
@@ -0,0 +1,12 @@
+﻿int m =-1, n =15;
+int sum = 0;
+
+for (int i = m; i <= n; i++)
+{
+    if (i>0)
+    {
+        sum+=i;
+    }
+}
+
+Console.WriteLine(sum);
---
+++ HW_9.2/Program.cs
@@ -0,0 +1,23 @@
+﻿double Akkerman(double m, double n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+    else
+    {
+        Console.WriteLine("Error! Enter a positive arguments");
+        return 0;
+    }
+    
+}
+
+Console.WriteLine(Akkerman(3, 2));
